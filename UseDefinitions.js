2d88316669ecf35e1e1e8d9164be10eb734d12fc
pkaljian <pkaljian@gmail.com>
2025-10-10 14:30:56 -0700

    Create UseDefinitions.js

---
+++ UseDefinitions.js
@@ -0,0 +1,95 @@
+/* UseDefinitions.js — verbatim definitions from your provided code text.
+   IMPORTANT: keys must exactly match your land-use names in zoningData.
+   You can add more entries below in the same format as needed.
+*/
+
+window.useDefinitions = {
+  "Artisan Shop": `A retail store selling art glass, ceramics, jewelry, and other handcrafted items, where the facility includes an area for the crafting of the items sold.`,
+
+  "Artisan/Craft Product Manufacturing": `An establishment manufacturing and/or assembling small products primarily by hand, including jewelry, pottery and other ceramics, as well as small glass and metal art and craft products. Includes taxidermists. May also include small scale artisan food and beverage product manufacturing and affiliated tasting rooms.`,
+
+  "Business Support Service": `An establishment within a building that provides services to other businesses. Examples of these services include:
+blueprinting; computer-related services (rental, repair); copying and quick printing services; film processing and photo finishing (retail); protective services (other than office related); security systems services.
+See also “Maintenance and Repair Services – Client Site Services.”`,
+
+  "Commercial Recreation Facility – Indoor": `Establishments providing indoor amusement and entertainment services for a fee or admission charge, including:
+bowling alleys; coin-operated amusement arcades; dance halls, clubs and ballrooms; electronic game arcades; ice skating and roller skating; pool and billiard rooms as primary uses
+This use does not include adult entertainment businesses. Four or more electronic games or coin-operated amusements in any establishment, or a premises where 50 percent or more of the floor area is occupied by amusement devices, are considered an electronic game arcade as described above, three or less machines are not considered a land use separate from the primary use of the site. See also “Theaters.”`,
+
+  "Commercial Recreation Facility – Outdoor": `Facilities for various outdoor recreational activities, where a fee is charged for use. Examples include:
+amusement and theme parks; go-cart tracks; golf driving ranges; miniature golf courses; water slides
+May also include commercial facilities customarily associated with the above outdoor commercial recreational uses, including bars and restaurants, video game arcades, etc.`,
+
+  "Community Meeting Facility": `A multi-purpose meeting and/or recreational facility typically consisting of one or more meeting or multi-purpose rooms, which may also include kitchen and/or outdoor cooking or eating facilities, that are available for use by various groups for such activities as meetings, parties, receptions, dances, etc.
+
+Community meeting facilities include community centers, and the following.
+
+1. Club, Lodge, Private Meeting Hall. Permanent, headquarters-type and meeting facilities for organizations operating on a membership basis for the promotion of the interests of the members, including facilities for:
+business associations; civic, social and fraternal organizations; labor unions and similar organizations; professional membership organizations; political organizations; other membership organizations
+
+2. Religious Facility. A facility operated by a religious organization to provide a place for worship, or the promotion of religious activities. This use includes:
+churches, mosques, synagogues, temples, etc.; accessory uses on the same site, including living quarters for the same type of land use permit required for the religious facility itself.
+May also include fund-raising sales, bazaars, dinners, parties, or other outdoor events on the same site. Other establishments maintained by religious organizations, including full-time educational institutions, hospitals and other potentially related operations (for examples, a recreational camp) are instead classified according to their respective activities.`,
+
+  "Drive-through Retail": `A facility where food and other products may be purchased by motorists without leaving their vehicles. Examples of these facilities include fast-food restaurants, drive-through coffee, dairy product, photo stores, pharmacies, etc.`,
+
+  "Drive-through Service": `A facility where services may be obtained by motorists without leaving their vehicles. These facilities include drive-through bank teller windows, dry cleaners, etc. Does not include: automated teller machines (ATMs), gas stations or other vehicle services, which are separately defined.`,
+
+  "Electronics, Equipment, and Appliance Manufacturing": `An establishment that manufactures equipment, apparatus, and /or supplies for the generation, storage, transmission, transformation and use of electrical energy, including:
+appliances including stoves/ovens, refrigerators, freezers, laundry equipment, fans, vacuum cleaners, sewing machines; audio and television receiving equipment; aviation instruments; computers, computer components, peripherals; electric welding apparatus; electrical transmission and distribution equipment; electronic components and accessories; industrial controls; instruments for measurement, testing, analysis and control, associated sensors and accessories; lighting and wiring equipment such as lamps and fixtures, wiring devices, vehicle lighting; motors and generators; miscellaneous electrical machinery, equipment and supplies such as batteries, X-ray apparatus and tubes, electromedical and electrotherapeutic apparatus, electrical equipment for internal combustion engines; optical instruments and lenses; photographic equipment and supplies; semiconductors, integrated circuits, related devices; storage media, blank and pre-recorded, including magnetic, magneto-optical, and optical products such as compact disks (CDs), diskettes and hard drives, digital versatile disks (DVDs), magnetic tape products, phonograph records ,etc.; surgical, medical and dental instruments; surveying and drafting equipment; telephone and telegraph apparatus; transformers, switchgear and switchboards; watches and clocks
+Does not include testing laboratories (soils, materials testing, etc.) (see “Laboratories”), or research and development facilities separate from manufacturing (see “Industrial Research and Development”).`,
+
+  "Fitness/Health Facility": `A fitness center, gymnasium, health and athletic club, which may include any of the following: sauna, spa or hot tub facilities; indoor tennis, handball, racquetball, archery and shooting ranges and other indoor sports activities. Does not include adult entertainment businesses.`,
+
+  "Food and Beverage Product Manufacturing": `Manufacturing establishments producing or processing foods and beverages for human consumption, and certain related products. Examples of these uses include:
+Bottling plants; breweries; candy, sugar and confectionery products manufacturing; catering services separate from stores or restaurants; coffee roasting; dairy products manufacturing; fats and oil product manufacturing; fruit and vegetable canning, preserving, related processing; grain mill products and by-products; meat, poultry and seafood canning, curing, byproduct processing; soft drink production; miscellaneous food item preparation from raw products; wineries.
+Does not include: bakeries, which are separately defined; beer brewing as part of a brewpub, bar or restaurant (see “Bar/Tavern,” and “Night Club”); or tasting rooms.`,
+
+  "Furniture/Fixtures Manufacturing, Cabinet Shop": `Manufacturers producing: wood and metal household furniture and appliances; bedsprings and mattresses; all types of office furniture and public building furniture and partitions, shelving, lockers and store furniture; and miscellaneous drapery hardware, window blinds and shades. Includes wood and cabinet shops, but not sawmills or planing mills, which are instead included under “Manufacturing – Heavy.”`,
+
+  "Gas Station": `A retail business selling gasoline or other motor vehicle fuels, and related products, derived from fossil fuels (e.g., petroleum, coal, natural gas).`,
+
+  "General Retail": `Stores and shops selling many lines of merchandise. Examples of these stores and lines of merchandise include:
+antique stores; art galleries, retail; artists’ supplies; auction rooms; bicycles; books, magazines, and newspapers; cameras and photographic supplies; clothing, shoes, and accessories; collectibles (cards, coins, comics, stamps, etc.); convenience stores; department stores; drug stores and pharmacies; dry goods; fabrics and sewing supplies; florists and houseplant stores (Indoor sales only (outdoor sales are “Building and Landscape Materials Sales); furniture, furnishings, and appliances; hobby materials; jewelry; luggage and leather goods; musical instruments, parts and accessories; orthopedic supplies; religious goods; second hand stores; small wares; specialty shops; sporting goods and equipment; stationery; toys and games; variety stores; video rental
+Does not include pawnshops. Does not include accessory retail uses—the retail sale of various products within a health care, hotel, office, or industrial complex to employees or customers, which are allowed accessory to all other related commercial uses. Examples of accessory retail uses include pharmacies, gift shops, and food service establishments within hospitals; and convenience stores and food service establishments within hotel, office and industrial complexes.`,
+
+  "Library, Museum": `Public or quasi-public facilities, examples of which include: aquariums, arboretums, art galleries and exhibitions, botanical gardens, historic sites and exhibits, libraries, museums, planetariums, and zoos. May also include accessory retail uses such as gift/book shop, restaurant, etc.`,
+
+  "Machinery Manufacturing": `An establishment that makes or processes raw materials into finished machines or parts for machines. Does not include the manufacture of electronics, equipment, or appliances (“Electronics, Equipment, and Appliance Manufacturing”).`,
+
+  "Media Production": `Facilities for motion picture, television, video, sound, computer, and other communications media production. These facilities include the following types.
+1. Back lots/outdoor facilities. Outdoor sets, back lots, and other outdoor facilities, including supporting indoor workshops and craft shops.
+2. Indoor support facilities. Administrative and technical production support facilities, including administrative and production offices, post-production facilities (editing and sound recording studios, foley stages, etc.), optical and special effects units, film processing laboratories, etc.
+3. Soundstages. Warehouse-type facilities providing space for the construction and use of indoor sets, including supporting workshops and craft shops.`,
+
+  "Metal Products Fabrication, Machine or Welding Shop": `An establishment engaged in the production and/or assembly of metal parts, including the production of metal cabinets and enclosures, cans and shipping containers, doors and gates, duct work, forgings and stampings, hardware and tools, plumbing fixtures and products, tanks, towers, and similar products. Examples of these uses include:
+blacksmith and welding shops; plating, stripping, and coating shops; sheet metal shops; machine shops and boiler shops`,
+
+  "Paper Product Manufacturing": `An establishment that converts per-manufactured paper or paperboard into boxes, envelopes, paper bags wallpaper, etc., and/or that coats or glazes premanufactured paper. Does not include the manufacturing of pulp, paper, or paperboard (see “Manufacturing – Heavy – Pulp and Pulp Product Manufacturing”).`,
+
+  "Plastics, other Synthetics, and Rubber Product Manufacturing": `The manufacture of rubber products including: tires, rubber footwear, mechanical rubber goods, heels and soles, flooring, and other rubber products from natural, synthetic, or reclaimed rubber. Also includes establishments engaged primarily in manufacturing tires; products from recycled or reclaimed plastics or Styrofoam; molding primary plastics for other manufacturers, manufacturing miscellaneous finished plastics products, fiberglass manufacturing, and fiber glass application services. Does not include establishments engaged primarily in recapping and retreading automobile tires (“Vehicle Services – major Repair/Body work”).`,
+
+  "Printing and Publishing": `A small-scale establishment engaged in printing by letterpress, lithography, gravure, screen, offset, or electrostatic (xerographic) copying; and other establishments serving the printing trade such as bookbinding, typesetting, engraving, photoengraving, and electrotyping. This use also includes establishments that publish newspapers, books and periodicals; establishments manufacturing business forms and binding devices. “Quick printing” services are included in the definition of “Business Support Services.”`,
+
+  "Research and Development (R&D)": `A facility for scientific research, and the design, development and testing of electrical, electronic, magnetic, optical and computer and telecommunications components in advance of product manufacturing, and the assembly of related products from parts produced off-site, where the manufacturing activity is secondary to the research and development activities. Includes pharmaceutical, chemical and biotechnology research and development. Does not include soils and other materials testing laboratories (see “Laboratory”), or medical laboratories (see “Medical Service – Clinic, Laboratory, Urgent Care”).`,
+
+  "School": `A public or private academic educational institution, including:
+boarding school; community college, college, or university; elementary, middle, and junior high schools; high school; military academy
+Also includes schools providing specialized education/training. Examples include the following:
+art school; ballet and other dance school; business, secretarial, and vocational school; computers and electronics school; drama school; driver education school; establishments providing courses by mail; language school; music school; professional school (law, medicine, etc.); seminaries/religious ministry training facility
+Also includes facilities, institutions and conference centers that offer specialized programs in personal growth and development, such as fitness, environmental awareness, arts, communications, and management. Does not include pre-schools and child day care facilities (see “Child Day Care Facilities”). See also the definition of “Studios – Art, Dance, Martial Arts, Music, etc.” for smaller-scale facilities offering specialized instruction.`,
+
+  "Small Product Manufacturing": `Establishments manufacturing and/or assembling small products primarily by hand, including manufacturing establishments producing small products not classified in another major manufacturing group, including: brooms and brushes; buttons, costume novelties; jewelry; musical instruments; pens, pencils, and other office and artists’ materials; sporting and athletic goods; toys; etc. See also “Artisan/Craft Product Manufacturing.”`,
+
+  "Stone and Cut Stone Product Manufacturing": `An establishment that cuts, shapes, and/or finishes marble, granite, slate, and/or other stone for construction and miscellaneous uses. Does not include establishments engaged primarily in buying or selling partly finished monuments and tombstones (“Handcraft Industries, Small Scale Manufacturing”)`,
+
+  "Structural Clay and Pottery Product Manufacturing": `An establishment that produces brick and structural clay products, including pipe, china plumbing fixtures, vitreous china parts, and/or fine earthenware and porcelain products. Does not include artist/ craftsman uses (see “Handcraft Industries and Small Scale Manufacturing,” “Home Occupations”).`,
+
+  "Tasting Room": `A facility or portion of a facility that supports a small-scale artisan craft brewery, distillery, or winery. The tasting room shall only serve and/or sell those beverages produced by the operating manufacturer and may operate at a separate location from the production facilities if under a duplicate ABC license type. The tasting room must also comply with all applicable ABC and State regulations for wine, beer, and distilled spirit manufacturers and tasting rooms.`,
+
+  "Veterinary Clinic, Animal Hospital": `Office and indoor medical treatment facilities used by veterinarians, including large and small animal veterinary clinics, and animal hospitals.`,
+
+  "Wholesaling and Distribution": `Establishments engaged in selling merchandise to retailers; to contractors, industrial, commercial, institutional, farm, or professional business users; to other wholesalers; or acting as agents or brokers in buying merchandise for or selling merchandise to such persons or companies. Examples of these establishments include:
+agents, merchandise or commodity brokers, and commission merchants; assemblers, buyers and associations engaged in the cooperative marketing of farm products; merchant wholesalers; stores primarily selling electrical, plumbing, heating and air conditioning supplies and equipment.
+Also includes storage, processing, packaging, and shipping facilities for mail order and e-commerce retail establishments.`
+};
